4eb16e7dd69652fd876a135d669a2b2cc4cb52a5
ZixuanZeng <zzeng23@uic.edu>
2017-03-13 22:33:53 -0500

    Background Interface
    Intend to edit

---
+++ Background.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+public interface Background {
+	// if there are some variables
+	void playBackgroundMusic();
+	void playGameMusic();
+	void paint(Graphics g);
+	void drawObstacles(int x, int y, int sizeLenght, Graphics g,
+			int type);
+	void run();
+	
+}
