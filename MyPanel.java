d5f376345f629605651c1e78a5a8eef2bbf505c5
ZixuanZeng <zzeng23@uic.edu>
2017-04-03 14:41:24 -0500

    Uplated MyPanel

--- MyPanel.java
+++ MyPanel.java
@@ -3,72 +3,71 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MyPanel extends JApplet implements Background, ActionListener{
-
+	
+	JFrame game;
+	JPanel myPanel;
+	JLabel gameName;
 	JToggleButton button1, button2, button3, button4;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Graphics graph = null;
-		/*
-		MyPanel panel = new MyPanel();
-		panel.setLayout(null);
-		panel.displayTutorial();
-		//panel.paint(graph);
-		panel.drawObstacles(0, 0, 1, graph, 2);
-		panel.playBackgroundMusic();
-		panel.playGameMusic();
-		panel.run();*/
+		
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
-				//new MyPanel();
 				new MyPanel().makeGUI();
 			}
 		});
 	}
 	
 	MyPanel(){
-		JFrame game = new JFrame  ("A game similar to Doodle Jump ---- Square-Jump");
+		
+		game = new JFrame  ("A game similar to Doodle Jump ---- Square-Jump");
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel myPanel = new JPanel();
+		myPanel = new JPanel();
 		myPanel.setOpaque(true);
 		myPanel.setBackground(Color.BLACK);
 		myPanel.setLayout(null);
 		
-		JLabel gameName = new JLabel("Welcom to    Square Jump", JLabel.CENTER);
+		gameName = new JLabel("Welcom to    Square Jump", JLabel.CENTER);
 		gameName.setSize(1000, 30);
         gameName.setLocation(5, 5);
 		gameName.setFont(new Font("Courier New", Font.ITALIC, 30));
 	    gameName.setForeground(Color.BLUE);
+	    myPanel.add(gameName);
 	    
 	    button1 = new JToggleButton("Start");
 	    button1.setSize(100, 30);
         button1.setLocation(400, 200);
+        myPanel.add(button1);
         
         button2 = new JToggleButton("Tutorial");
         button2.setSize(100,30);
         button2.setLocation(400,300);
+        myPanel.add(button2);
+        button2.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent arg0){
+        		JOptionPane.showMessageDialog(null, "Press 'A' or 'D' to control.");
+        	}
+        });
         
         button3 = new JToggleButton("Score Board");
-        button3.setSize(100,30);
-        button3.setLocation(400,400);
+        button3.setSize(150,30);
+        button3.setLocation(375,400);
+        myPanel.add(button3);
         
         button4 = new JToggleButton("Quit");
+        button4.addActionListener(e->System.exit(0));
         button4.setSize(100,30);
         button4.setLocation(400,500);
+        myPanel.add(button4);
         
-	    myPanel.add(button1);
-	    myPanel.add(button2);
-	    myPanel.add(button3);
-	    myPanel.add(button4);
-	    myPanel.add(gameName);
 	    game.setContentPane(myPanel);
 	    game.setSize(1000,800);
-	    //game.setLocationByPlatform(true);
-		//game.setLayout(new FlowLayout());
-	    //game.add(gameName);
 		game.setVisible(true);
 	}
+	// Suppose to use in Swing later
 	
 	public void init(){
 		try{
@@ -85,15 +84,27 @@ public class MyPanel extends JApplet implements Background, ActionListener{
 	private void makeGUI(){
 		setLayout(new FlowLayout());
 		button1 = new JToggleButton("Start");
+		button4 = new JToggleButton("Quit");
 		button1.addItemListener(new ItemListener(){
 			public void itemStateChanged(ItemEvent ie){
 				if(button1.isSelected()){
 					
 				}
+				
+			}
+		});
+		
+		button4.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent ie){
+				if(button4.isSelected()){
+					System.exit(0);
+				}
+				
 			}
 		});
 		
 		add(button1);
+		add(button4);
 	}
 
 	@Override
