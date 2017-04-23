e4f8406fa8f9e56ed09b6c03292beea8e5f2ea6d
ZixuanZeng <zzeng23@uic.edu>
2017-04-23 15:41:00 -0500

    Added Monster class

---
+++ Monster.java
@@ -0,0 +1,45 @@
+import javafx.scene.paint.Color;
+
+public class Monster extends Shape{
+	
+	public Monster(int xPos, int yPos) {
+		Height = 15;
+		Width = 50;
+		XPos = xPos;
+		YPos = yPos;
+		color = Color.RED;
+		
+	}
+
+	@Override
+	public int getXPos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getYPos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
