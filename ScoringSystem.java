a3f4e009a421276ced6d3e25a852b48d1390c073
ZixuanZeng <zzeng23@uic.edu>
2017-04-25 12:21:09 -0500

    Added score to score button

--- ScoringSystem.java
+++ ScoringSystem.java
@@ -4,13 +4,12 @@ import javafx.scene.canvas.GraphicsContext;
 
 public interface ScoringSystem {
 	
-	final int winningScore = 10000;
+	final int winningScore = 1000;
 	int initialScore = 0;
 	
 	void scoringMechanics();
 	boolean isWin();
 	void receiveDamaged();
-	void awardPoints();
-	int getScore();
-	public void addDistance(int dis);
+	int awardPoints();
+	void drawScore();
 }
